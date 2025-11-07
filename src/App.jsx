e405047719f31c768d31b4430c1049ae0bfb7271
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-08 00:04:44 +0530

    Exercise 3 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
+import JSXRules from "./components/JSXRules";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
+	const myName = "Raja";
+	const multiply = (a, b) => a * b;
+	const specialClass = "simple-class";
+
 	return (
 		<div>
-			<Header />
-			<MainContent />
-			<Footer />
+			<WelcomeMessage />
+			<JSXRules />
+			<section>
+				<h2>2 + 2{2 + 2}</h2>
+				<h2>{myName}</h2>
+				<h3>My Friends list: {["Alex", "John", "Waheed", "Jordan"]}</h3>
+				<h3>{multiply(3, 4)}</h3>
+				<p className={specialClass}>This is special class</p>
+			</section>
 		</div>
 	);
 };
