c2e2a85796de39ab7c631c45187bc5241e6449c7
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-09 17:13:58 +0530

    Prop Exercise Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import ProductList from "./components/ProductList";
-import UserListV2 from "./components/UserListV2";
+import Person from "./components/Person";
+import Product from "./components/Product";
 
 const App = () => {
 	return (
 		<main>
-			<h2>User List</h2>
-			<UserListV2 />
-			<br />
-			<br />
-			<h2>Product List</h2>
-			<ProductList />
+			<h1>Props Exercise</h1>
+			<section>
+				<h2>Person</h2>
+				<Person name={"Raja"} age={22} />
+				<Person name={"Nubi"} age={18} />
+			</section>
+			<section>
+				<h2>Product</h2>
+				<Product name={"Laptop"} price={120000} />
+				<Product name={"IPhone"} price={80000} />
+			</section>
 		</main>
 	);
 };
