1b9f9abdb91cf2cfeae6f15890bf5bead0420e50
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-14 19:08:53 +0530

    useContext exercise complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import BasicEffect from "./components/BasicEffect";
-import CounterEffect from "./components/CounterEffect";
-import FetchDataEffect from "./components/FetchDataEffect";
+import UserProfile from "./components/UserProfile";
+import { UserProvider } from "./components/UserContext";
+import UpdateUser from "./components/UpdateUser";
 
 const App = () => {
 	return (
-		<main>
-			<FetchDataEffect />
-			<CounterEffect />
-			<BasicEffect />
-		</main>
+		<UserProvider>
+			<UserProfile />
+			<UpdateUser />
+		</UserProvider>
 	);
 };
 
