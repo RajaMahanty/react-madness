d574573f38429bae012d046c8dc2d8617f6cf8b8
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-17 19:44:32 +0530

    Create Product component structure
    Created the dummy product structure and added a card, will later refactor it.

--- project/e-com-filter/src/components/Products/Products.jsx
+++ project/e-com-filter/src/components/Products/Products.jsx
@@ -1,7 +1,41 @@
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
 import "./Products.css";
 
 const Products = () => {
-	return <div>Product</div>;
+	return (
+		<>
+			<section className="card-container">
+				<section className="card">
+					<img
+						src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+						alt="Nike Air Monarch IV"
+						className="card-image"
+					/>
+					<div className="card-details">
+						<h3 className="card-title">Nike Air Monarch IV</h3>
+						<section className="card-reviews">
+							<AiFillStar className="ratings-start" />
+							<AiFillStar className="ratings-start" />
+							<AiFillStar className="ratings-start" />
+							<AiFillStar className="ratings-start" />
+							<span className="total-reviews">4</span>
+						</section>
+
+						<section className="card-price">
+							<div className="price">
+								<del>$300</del> 200
+							</div>
+
+							<div className="bag">
+								<BsFillBagHeartFill className="bag-icon" />
+							</div>
+						</section>
+					</div>
+				</section>
+			</section>
+		</>
+	);
 };
 
 export default Products;
